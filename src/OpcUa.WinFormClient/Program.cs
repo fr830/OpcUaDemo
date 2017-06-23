e447c454c433914cf829e9b730872a782092247f
jiangming <jiangming@tongfangcloud.com>
2017-06-23 10:29:48 +0800

    可以运行

--- src/OpcUa.WinFormClient/Program.cs
+++ src/OpcUa.WinFormClient/Program.cs
@@ -36,7 +36,59 @@ namespace TongFang.OpcUa.Client
             {
                 ApplicationName = "Data Access Client",
                 ApplicationType = ApplicationType.Client,
-                 ApplicationUri = 
+                ApplicationUri = "urn:localhost:UASDK:DataAccessClient",
+                CertificateValidator = new CertificateValidator
+                {
+                },
+                ClientConfiguration = new ClientConfiguration
+                {
+                     DefaultSessionTimeout = 60000,
+                     DiscoveryServers = new EndpointDescriptionCollection
+                     {
+                          new EndpointDescription("opc.tcp://127.0.0.1:4840"),
+                          new EndpointDescription("http://127.0.0.1:52601/UADiscovery"),
+                          new EndpointDescription("http://127.0.0.1/UADiscovery/Default.svc"),
+                     },
+                     MinSubscriptionLifetime = 10000,
+                },
+                SecurityConfiguration = new SecurityConfiguration
+                {
+                    ApplicationCertificate = new CertificateIdentifier
+                    {
+                        StoreType = "X509Store",
+                        StorePath = "CurrentUser\\UA_MachineDefault",
+                        SubjectName = "UA Core Sample Client"
+                    },
+                    TrustedPeerCertificates = new CertificateTrustList
+                    {
+                        StoreType = "Directory",
+                        StorePath = "OPC Foundation/CertificateStores/UA Applications",
+                    },
+                    TrustedIssuerCertificates = new CertificateTrustList
+                    {
+                        StoreType = "Directory",
+                        StorePath = "OPC Foundation/CertificateStores/UA Certificate Authorities",
+                    },
+                    RejectedCertificateStore = new CertificateTrustList
+                    {
+                        StoreType = "Directory",
+                        StorePath = "OPC Foundation/CertificateStores/RejectedCertificates",
+                    },
+                    NonceLength = 32,
+                    AutoAcceptUntrustedCertificates = true
+                },
+                TransportConfigurations = new TransportConfigurationCollection(),
+                TransportQuotas = new TransportQuotas
+                {
+                    OperationTimeout = 600000,
+                    MaxStringLength = 1048576,
+                    MaxByteStringLength = 1048576,
+                    MaxArrayLength = 65535,
+                    MaxMessageSize = 4194304,
+                    MaxBufferSize = 65535,
+                    ChannelLifetime = 300000,
+                    SecurityTokenLifetime = 3600000
+                }
             };
             return config;
         }
