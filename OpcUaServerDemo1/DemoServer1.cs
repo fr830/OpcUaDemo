4adfc3579cfb7049904851c01019895b1fe39671
jiangming <jiangming@tongfangcloud.com>
2017-06-29 16:26:20 +0800

    add Demo1 Server

--- OpcUaServerDemo1/DemoServer1.cs
+++ OpcUaServerDemo1/DemoServer1.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Opc.Ua;
 using Opc.Ua.Server;
 
 namespace OpcUaServerDemo1
 {
     public class DemoServer1 : StandardServer
     {
+        public IList<Uri> ServerUris
+        {
+            get { return BaseAddresses.Select(x => x.Url).ToArray(); }
+        }
     }
 }
--- OpcUaServerDemo1/Program.cs
+++ OpcUaServerDemo1/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using Opc.Ua;
+using Opc.Ua.Server;
 
 namespace OpcUaServerDemo1
 {
@@ -6,7 +11,66 @@ namespace OpcUaServerDemo1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Task<ApplicationConfiguration> t = LoadApplicationConfiguration();
+                t.Wait();
+                ApplicationConfiguration config = t.Result;
+                if (!config.SecurityConfiguration.AutoAcceptUntrustedCertificates)
+                {
+                    config.CertificateValidator.CertificateValidation += new CertificateValidationEventHandler(CertificateValidator_CertificateValidation);
+                }
+                DemoServer1 server = new DemoServer1();
+                server.Start(config);
+                Console.WriteLine("{0}Server has Started! Uri:", config.ApplicationName);
+                var uris = server.ServerUris;
+                foreach (var uri in uris)
+                {
+                    Console.WriteLine(uri.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        public static async Task<ApplicationConfiguration> LoadApplicationConfiguration()
+        {
+            string filePath = ApplicationConfiguration.GetFilePathFromAppConfig("Demo1Server");
+            ApplicationConfiguration configuration = await ApplicationConfiguration.Load("Demo1Server", ApplicationType.Server);
+
+            if (configuration == null)
+            {
+                throw ServiceResultException.Create(StatusCodes.BadConfigurationError, "Could not load configuration file.");
+            }
+            FixupAppConfig(configuration);
+            return configuration;
+        }
+
+        public static ApplicationConfiguration FixupAppConfig(
+            ApplicationConfiguration configuration)
+        {
+            configuration.ApplicationUri = Utils.ReplaceLocalhost(configuration.ApplicationUri);
+            if (configuration.ServerConfiguration != null)
+            {
+                for (int i = 0; i < configuration.ServerConfiguration.BaseAddresses.Count; i++)
+                {
+                    configuration.ServerConfiguration.BaseAddresses[i] =
+                        Utils.ReplaceLocalhost(configuration.ServerConfiguration.BaseAddresses[i]);
+                }
+            }
+            return configuration;
+        }
+
+        private static void CertificateValidator_CertificateValidation(CertificateValidator validator, CertificateValidationEventArgs e)
+        {
+            Debug.WriteLine("Accepted Certificate: {0}", e.Certificate.Subject);
+            e.Accept = (e.Error.StatusCode == StatusCodes.BadCertificateUntrusted);
         }
     }
 }
